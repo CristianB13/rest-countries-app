a78c80425a7dd6e6108ebe829c5ee0b6c624af19
CristianB13 <cristian36200119@gmail.com>
2023-01-14 23:33:03 +0200

    add header and toggle theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  host: {'[class.dark]': 'darkMode'}
 })
 export class AppComponent {
   title = 'rest-countries-app';
+  darkMode: boolean = true;
 }
