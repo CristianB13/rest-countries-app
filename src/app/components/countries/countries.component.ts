05d64266d5c15e130772940de58d41cd660489a7
CristianB13 <cristian36200119@gmail.com>
2023-01-18 19:11:39 +0200

    add search and filter options to countries component

--- src/app/components/countries/countries.component.ts
+++ src/app/components/countries/countries.component.ts
@@ -9,9 +9,27 @@ import { CountryService, Country } from 'src/app/services/country.service';
 })
 export class CountriesComponent implements OnInit {
   countries$!: Observable<Country[]>;
+  filterOptions: string[] = ["Africa", "Americas", "Asia", "Europe", "Oceania"];
+  selectedRegions: Set<string> = new Set<string>;
   constructor(private countryService: CountryService) { }
 
   ngOnInit(): void {
     this.countries$ = this.countryService.getAll();
+  }
+
+  searchCountry(value: string): void {
+    if (value === "") {
+      this.countries$ = this.countryService.getAll();
+    } else {
+      this.countries$ = this.countryService.getByName(value);
+    }
+  }
+
+  filterByRegionsFunc(regions: Set<string>): (item: any) => boolean   {
+    return (item) => {
+      if (regions.size === 0) return true;
+      if (regions.has(item.region)) return true;
+      return false;
+    }
   }
 }
--- src/app/components/country/country.component.ts
+++ src/app/components/country/country.component.ts
@@ -1,6 +1,6 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Component, HostBinding, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
 import { Country, CountryService } from 'src/app/services/country.service';
 import { Location } from '@angular/common';
 import { Title } from '@angular/platform-browser';
@@ -10,11 +10,13 @@ import { Title } from '@angular/platform-browser';
   templateUrl: './country.component.html',
   styleUrls: ['./country.component.scss']
 })
-export class CountryComponent implements OnInit {
+export class CountryComponent implements OnInit, OnDestroy {
   country$!: Observable<Country[]>;
+  private subscription!: Subscription;
 
   constructor(private countryService: CountryService,
     private route: ActivatedRoute,
+    private router: Router,
     private location: Location,
     private title: Title) { }
 
@@ -22,12 +24,18 @@ export class CountryComponent implements OnInit {
     this.route.paramMap.subscribe(params => {
       this.country$ = this.countryService.getByCode(params.get("code") || "")
     })
-    this.country$.subscribe(country => {
+    this.subscription = this.country$.subscribe(country => {
       this.title.setTitle(`RestCountriesApp | ${country[0].name.common}`)
+    }, error => {
+      this.router.navigate([""], {replaceUrl: true});
     })
   }
 
   back(): void {
     this.location.back();
+  }
+  
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
